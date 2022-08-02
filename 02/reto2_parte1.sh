019687f43f4e1b598c172b8ffa41356b982dcf8b
daneils-blacknet <danielsanti.ar@gmail.com>
2022-08-02 20:21:22 -0300

    Inicio Reto 2 - cambio de plataforma

---
+++ 02/reto2_parte1.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+## Parte 1
+mkdir archivos; cd archivos
+touch archiv{1..9}
+chmod a-rwx archiv*
---
+++ 02/reto2_parte2.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+## Parte 1
+mkdir archivos; cd archivos
+touch archv{1..9}
+chmod a-rwx archiv*
